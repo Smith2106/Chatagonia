ce07b138b15a3dc84b74b04a6d9647ad6fbbbfe3
Elijah Smith <elijahs2106@gmail.com>
2018-07-03 15:25:36 -0400

    Style sidebar, chat header, and chat components

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -2,13 +2,35 @@ import React from 'react';
 
 const ChatHeader = () => {
     return (
-        <header className="ChatHeader">
+        <header className="ChatHeader" style={styles.chatHeader}>
             <div className="roomInfo">
-                <h2>#general</h2>
-                <p>Announcements and general chat</p>
+                <h2 style={styles.roomInfoH2}>#general</h2>
+                <p styles={styles.roomInfoP}>Announcements and general chat</p>
             </div>
         </header>
     );
+};
+
+const styles = {
+    chatHeader: {
+        backgroundColor: '#f3f3f3',
+        borderBottom: '1px solid #ccc',
+        height: '3rem',
+        padding: '0 1rem',
+        display: 'flex',
+        alignItems: 'center',
+    },
+
+    roomInfoH2: {
+        fontSize: '1.1rem',
+        margin: '0',
+    },
+
+    roomInfoP: {
+        color: '#999',
+        margin: '0',
+        fontSize: '0.8rem',
+    },
 };
 
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,21 +3,26 @@ import React from 'react';
 const Sidebar = (props) => {
     return (
         <aside className="Sidebar" style={styles.sidebar}>
-            <div className="UserInfo" style={styles.children}>
+            <div className="UserInfo" style={{...styles.children, ...styles.userInfo}}>
                 <div
-                className="Avatar"
+                    className="Avatar"
+                    styles={styles.userAvatar}
                 ></div>
-                <div className="user">{props.user.displayName}</div>
-                <a href="#">
-                <i className="fas fa-sign-out-alt"></i>
+                <div className="user" style={styles.user}>{props.user.displayName}</div>
+                <a href="#" style={styles.userInfoA}>
+                    <i className="fas fa-sign-out-alt"></i>
                 </a>
             </div>
             <h1 style={styles.h1}>XTBC 18</h1>
             <nav className="RoomList" style={styles.children}>
-                <h2>Rooms</h2>
-                <ul>
-                <li><a href="#">general</a></li>
-                <li><a href="#">random</a></li>
+                <h2 style={styles.roomlistH2}>Rooms</h2>
+                <ul style={styles.roomlistUl}>
+                    <li style={styles.roomlistLi}>
+                        <a style={styles.roomlistA} href="#">general</a>
+                    </li>
+                    <li style={styles.roomlistLi}>
+                        <a style={styles.roomlistA} href="#">random</a>
+                    </li>
                 </ul>
             </nav>
         </aside>
@@ -33,15 +38,60 @@ const styles = {
         display: 'flex',
         flexDirection: 'column',
     },
+
     children: {
         padding: '0 1rem',
     },
+
     h1: {
         color: 'white',
         fontSize: '1.2rem',
         marginTop: '0',
         padding: '0 1rem',
     },
+
+    roomlistH2: {
+        fontSize: '1rem',
+    },
+
+    roomlistUl: {
+        listStyle: 'none',
+        marginLeft: '0',
+        paddingLeft: '0',
+    },
+
+    roomlistLi: {
+        marginBottom: '0.5rem',
+    },
+
+    roomlistA: {
+        display: 'block',
+        color: 'whitesmoke',
+        textDecoration: 'none',
+    },
+
+    userInfo: {
+        marginBottom: '1rem',
+        display: 'flex',
+        alignItems: 'center',
+    },
+
+    userAvatar: {
+        marginRight: '0.5rem',
+    },
+
+    user: {
+        flex: '1',
+    },
+
+    userInfoA: {
+        border: '0',
+        padding: '0',
+        backgroundColor: 'transparent',
+        color: 'rgba(255, 255, 255, 0.6)',
+        fontSize: '1.2rem',
+        transition: 'color 0.25s ease-out',
+    }
 };
 
 export default Sidebar;
