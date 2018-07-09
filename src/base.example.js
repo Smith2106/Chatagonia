0de32449eb9508130cd8145f1997772caaefab42
Elijah Smith <elijahs2106@gmail.com>
2018-07-09 09:39:38 -0400

    Update base example and add google authentication setup

--- src/base.example.js
+++ src/base.example.js
@@ -1,4 +1,7 @@
 import firebase from 'firebase/app';
+import 'firebase/database';
+import Rebase from 're-base';
+import 'firebase/auth';
 
 // Initialize firebase
 const config = {
@@ -8,6 +11,17 @@ const config = {
     projectId: "YOUR PROJECT ID",
     storageBucket: "YOUR STORAGE BUCKET",
     messagingSenderId: "YOUR MESSAGING SENDER ID"
-  };
+};
 
-  const app = firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+
+// Configure authentication
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+auth.setPersistence(firebase.auth.Auth.Persistence.SESSION);
+
+// Configure database
+const db = firebase.database(app);
+const base = Rebase.createClass(db);
+
+export { app, base, auth, googleProvider };
